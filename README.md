1c3e3c9b5e01c4d6c0d64c55cc3f80d3038d66af
Tomasz Kuc <contact@tomaszjankuc.com>
2024-10-06 22:05:31 +0200

    Add seeds and improve README

--- README.md
+++ README.md
@@ -13,3 +13,25 @@ Participant can:
 * log in;
 * register to an event;
 * see a list of all events that he is regestered to.
+
+## Setup
+
+1. Run `git clone git@github.com:tjku/event-registration-system.git` to download repository.
+
+2. Install Ruby 3.3.5 version.
+   To do it, you can use [RVM](https://rvm.io/) or [rbenv](https://github.com/rbenv/rbenv/).
+
+3. Install SQLite3.
+
+4. Create and migrate the database:
+
+   ```
+   bundle exec rails db:create
+   bundle exec rails db:migrate
+   ```
+
+5. Seed the database:
+
+   ```
+   bundle exec rails db:seed
+   ```
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+organizer1 = User.find_or_initialize_by(email: 'organizer1@example.com', role: :organizer)
+organizer1.password = organizer1.password_confirmation = 'qwerty'
+organizer1.save! if organizer1.new_record?
+
+organizer2 = User.find_or_initialize_by(email: 'organizer2@example.com', role: :organizer)
+organizer2.password = organizer2.password_confirmation = 'qwerty'
+organizer2.save! if organizer2.new_record?
+
+participant = User.find_or_initialize_by(email: 'participant@example.com', role: :participant)
+participant.password = participant.password_confirmation = 'qwerty'
+participant.save! if participant.new_record?
